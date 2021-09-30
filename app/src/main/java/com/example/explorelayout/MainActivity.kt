b1cfb166f546ce3abe2797dc0c6d4cbac51ab1f1
hello_world <haileformal@gmail.com>
2021-09-30 19:16:57 +0300

    Basic functionality like navigation, Done

---
+++ app/src/main/java/com/example/explorelayout/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.explorelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+    }
+
+    fun linearLayout(v: View) {
+        setContentView(R.layout.linear_layout)
+    }
+}
